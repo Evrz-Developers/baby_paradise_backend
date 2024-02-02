ae6798ce4133f19db41fc7f8a0950d48c64e7e46
Robin-Mobiux <robin@mobiux.in>
2024-02-02 20:29:09 +0530

    add duplicate option in backend

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
 from shop.models import Product, Category
+from django.utils.translation import ngettext
+from django.contrib import messages
 
 # 3. REGISTER THE MODELS
-admin.site.register(Product)
 admin.site.register(Category)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    actions = ['duplicate_selected']
+
+    def duplicate_selected(self, request, queryset):
+        for product in queryset:
+            # Clear primary key to create a new instance
+            product.pk = None
+            product.save()
+
+        self.message_user(
+            request,
+            ngettext(
+                'Successfully duplicated %(count)d product.',
+                'Successfully duplicated %(count)d products.',
+                queryset.count(),
+            ) % {'count': queryset.count()},
+            messages.SUCCESS,
+        )
+
+    duplicate_selected.short_description = "Duplicate selected products"
+
+admin.site.register(Product, ProductAdmin)
